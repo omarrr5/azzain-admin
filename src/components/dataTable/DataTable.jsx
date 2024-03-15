d626bb8a7d9cddb95a562e8400852edb367af4ee
omar tarik yehya <khalaf.omar20@gmail.com>
2024-03-15 15:42:13 +0800

    feat: added refill button to datatable

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Box, Chip } from '@mui/material';
+import { Box, Chip, Button } from '@mui/material';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import './datatable.scss';
+
+const handleDelete = (id) => {
+  // delete the item
+  // mutation.mutate(id)
+};
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 90 },
@@ -19,8 +24,20 @@ const columns = [
     field: 'location',
     headerName: 'Location',
     width: 100
-  }
-  ,
+  },
+  {
+    field: 'papers',
+    headerName: 'Remaining Papers',
+    width: 150,
+    renderCell: (params) => (
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div>{params.value}</div>
+        <Button variant="contained" color="primary" size="small" style={{ marginLeft: '15px' }}>
+          Refill
+        </Button>
+      </div>
+    ),
+  },
   {
     field: 'createdAt',
     headerName: 'Created At',
@@ -49,14 +66,9 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, name: 'PIXMA G4770', assocUser: 'omarrr5', location: 'Cyberjaya' , createdAt: '2024-03-08 14:20:22', status: 'yes' },
-  { id: 2, name: 'HP Smart Tank 750 All-in-One Printer', assocUser: 'admin', location: 'Melaka' , createdAt: '2024-03-08 14:20:22', status: 'No' }
+  { id: 1, name: 'PIXMA G4770', assocUser: 'omarrr5', location: 'Cyberjaya', papers: '300' , createdAt: '2024-03-08 14:20:22', status: 'yes' },
+  { id: 2, name: 'HP Smart Tank 750 All-in-One Printer', assocUser: 'admin', location: 'Melaka', papers: '400' , createdAt: '2024-03-08 14:20:22', status: 'No' }
 ];
-
-const handleDelete = (id) => {
-  // delete the item
-  // mutation.mutate(id)
-};
 
 function DataTable() {
   return (
